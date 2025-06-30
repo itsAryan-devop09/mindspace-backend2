5e07160593d8144034bcd033ea0acc1939bb7e5c
itsAryan-devop09 <thearyanfuture90@gmail.com>
2025-06-30 16:07:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -215,4 +215,5 @@ def submit_checkin():
 
 # Step 10: Run App
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
